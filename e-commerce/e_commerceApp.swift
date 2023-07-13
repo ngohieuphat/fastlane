7963202987c30f0c35bd320c665e5428ba796d97
ngohieuphat <ngohipha1@gmail.com>
2023-07-13 11:07:21 +0700

    Initial Commit

---
+++ e-commerce/e_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerceApp.swift
+//  e-commerce
+//
+//  Created by Ngo Hieu Phat on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct e_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ e-commerceUITests/e_commerceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  e_commerceUITestsLaunchTests.swift
+//  e-commerceUITests
+//
+//  Created by Ngo Hieu Phat on 13/07/2023.
+//
+
+import XCTest
+
+class e_commerceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
